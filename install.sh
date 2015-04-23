c0200e2caee4ccc26fc731c9f23f0ec067a8c484
yake <yake.abc@gmail.com>
2015-04-23 23:08:38 +0800

    use path of current directory in install.sh instead of hard-code

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-mv ../tmux-yake ../.tmux-yake
-ln -sf ~/.tmux-yake/.tmux.conf ~/.tmux.conf
+#!/usr/bin/env bash
+
+current_dir=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+ln -sf ${current_dir}/.tmux.conf ~/.tmux.conf
